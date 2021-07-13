0289e8e091680fc26ae07741b2a03d036448f388
Hyunsu1227 <vhtmxmqhdl@gmail.com>
2021-07-13 09:25:21 +0900

    add grading.js

--- examples-browser/public/js/home.js
+++ examples-browser/public/js/home.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     getShortAnswerQuestion();
-    timer();
 })
 
 var question;
@@ -47,26 +46,4 @@ function navClick(i){
         </div>
     `)
     $(".main").append(`<h2>${question[i].description}</h2>`)
-}
-
-function timer(){
-    var time = 600; //기준시간 작성
-	var min = ""; //분
-	var sec = ""; //초
-
-	//setInterval(함수, 시간) : 주기적인 실행
-	var x = setInterval(function() {
-		//parseInt() : 정수를 반환
-		min = parseInt(time/60); //몫을 계산
-		sec = time%60; //나머지를 계산
-
-		document.getElementById("timer").innerHTML = min + "분" + sec + "초";
-		time--;
-
-		//타임아웃 시
-		if (time < 0) {
-			clearInterval(x); //setInterval() 실행을 끝냄
-			document.getElementById("timer").innerHTML = "시간초과";
-		}
-	}, 1000);
 }
